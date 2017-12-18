d39f5e09842f9ad6337933f53a8566072b4d790b
Francis Jervis <francis@timesfree.co>
2017-12-18 14:09:38 -0800

    Update Annotation.swift

--- MapboxARKit/Annotation.swift
+++ MapboxARKit/Annotation.swift
@@ -5,10 +5,11 @@ public class Annotation: NSObject {
     public var location: CLLocation
     public var calloutImage: UIImage?
     public var anchor: MBARAnchor?
-    
-    public init(location: CLLocation, calloutImage: UIImage?) {
+    public var identifier: String
+    public init(location: CLLocation, calloutImage: UIImage?, identifier: String) {
         self.location = location
         self.calloutImage = calloutImage
+        self.identifier = identifier
     }
     
 }
