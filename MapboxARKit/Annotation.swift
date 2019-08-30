b9f54b6d65c181a7e56ab116b472575cf3f90527
Seungyong Kwak <guieenoutis@gmail.com>
2019-08-30 19:09:00 +0900

    Add weak keyword to every property on Annotation

--- MapboxARKit/Annotation.swift
+++ MapboxARKit/Annotation.swift
@@ -2,9 +2,9 @@ import CoreLocation
 
 open class Annotation: NSObject {
     
-    public var location: CLLocation
-    public var calloutImage: UIImage?
-    public var anchor: MBARAnchor?
+    public weak var location: CLLocation?
+    public weak var calloutImage: UIImage?
+    public weak var anchor: MBARAnchor?
     
     public init(location: CLLocation, calloutImage: UIImage?) {
         self.location = location
